b2b102db1f3e17d10bf45b533d27f5d74427a551
Artyom Kolbun <kolbunartema@gmail.com>
2020-06-08 17:01:33 +0700

    Add function enter

---
+++ src/enter.c
@@ -0,0 +1,18 @@
+#include "getch.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+void enter(int *m) {
+  char out[1000];
+  int c, i = 0;
+  while ((c = getch()) != 10) {
+    if (isdigit(c)) {
+      if ((char)c != '\n')
+        out[i++] = (char)c;
+      printf("%c", c);
+    }
+  }
+  printf("\n");
+  out[i] = '\0';
+  *m = atoi(out);
+}
