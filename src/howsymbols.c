757866d1615f1a47cde464147369fb9b9f244d0d
Artyom Kolbun <kolbunartema@gmail.com>
2020-06-08 14:31:03 +0700

    FIx howsymbols.c

--- src/howsymbols.c
+++ src/howsymbols.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-int howsymbols(FILE *f) {
-  int count = 0;
-  if (f == NULL) {
-    perror("");
-    exit(1);
-  }
-  while (!feof(f)) {
-    getc(f);
-    count++;
-  }
-  count -= 2;
-  rewind(f);
-  return count;
+int howsymbols(FILE* f)
+{
+    int count = 0;
+    if (f == NULL) {
+        perror("");
+        return -1;
+    }
+    while (!feof(f)) {
+        getc(f);
+        count++;
+    }
+    count -= 2;
+    rewind(f);
+    return count;
 }
