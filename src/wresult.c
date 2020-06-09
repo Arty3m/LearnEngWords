4ace6e017eb16164269bb71af8a625c86614a303
ilushaaaa <simbirovilya@gmail.com>
2020-06-09 16:37:20 +0700

    Add finc wresult

---
+++ src/wresult.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void wresult(int strok, int *p) {
+  int i = 0;
+  FILE *f = fopen("Results.txt", "w");
+  for (i = 0; i < strok; i++) {
+    fprintf(f, "%d\n", p[i]);
+  }
+  rewind(f);
+  fclose(f);
+}
---
+++ src/wresult.h
@@ -0,0 +1,4 @@
+#ifndef WRESULT_H
+#define WRESULT_H
+void wresult(int strok, int* p);
+#endif
