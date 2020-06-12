1d6c2b8d2cfdb3bc80b1a5311212d4a353c44cb0
Artyom Kolbun <kolbunartema@gmail.com>
2020-06-12 13:25:26 +0700

    Add test for howstring

--- test/main-test.c
+++ test/main-test.c
@@ -1,5 +1,34 @@
 #define CTEST_MAIN
 #include <ctest.h>
+#include <howstring.h>
+CTEST(howstring, correct_1)
+{
+    FILE* f;
+    f = fopen("test/testfiles/test1.txt", "r");
+    int result = 0;
+    result = howstring(f);
+    rewind(f);
+    fclose(f);
+    ASSERT_EQUAL(4, result);
+}
+CTEST(howstring, correct_2)
+{
+    FILE* f;
+    f = fopen("test/testfiles/test1_1.txt", "r");
+    int result = 0;
+    result = howstring(f);
+    rewind(f);
+    fclose(f);
+    ASSERT_EQUAL(4, result);
+}
+CTEST(howstring, nonexistentfile)
+{
+    FILE* f;
+    f = fopen("test/testfiles/filenotexist.txt", "r");
+    int result = 0;
+    result = howstring(f);
+    ASSERT_EQUAL(-1, result);
+}
 int main(int argc, const char* argv[])
 {
     return ctest_main(argc, argv);
