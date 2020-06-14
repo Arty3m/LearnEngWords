fa897004759c537dc2bc0d00da249088455be924
Artyom Kolbun <kolbunartema@gmail.com>
2020-06-14 15:53:53 +0700

    Fix function readres

--- src/readres.c
+++ src/readres.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-void readres(int strok, int *p) {
-  int i = 0;
-  char s[100];
-  FILE *f = fopen("Results.txt", "r");
-  if (f == NULL) {
-    perror("Ошибка открытия файла");
-    // return -1;
-  }
-  for (i = 0; i < strok; i++) {
-    if (fgets(s, 30, f) != NULL)
-      p[i] = atoi(s);
-  }
-  rewind(f);
-  fclose(f);
+int readres(FILE* f, int strok, int* p)
+{
+    int i = 0;
+    char s[100];
+    if (f == NULL) {
+        perror("");
+        return -1;
+    }
+    for (i = 0; i < strok; i++) {
+        if (fgets(s, 30, f) != NULL)
+            p[i] = atoi(s);
+    }
+    rewind(f);
+    fclose(f);
+    return 0;
 }
